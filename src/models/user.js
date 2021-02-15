aad8030b654348159b7791f7e8b6d1bc75342d66
Sang Nguyen <sangnn2012@gmail.com>
2021-02-15 15:53:44 +0700

    fix

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -21,6 +22,7 @@ const userSchema = new mongoose.Schema({
   },
   email: {
     type: String,
+    unique: true,
     required: true,
     trim: true,
     lowercase: true,
@@ -38,9 +40,43 @@ const userSchema = new mongoose.Schema({
         throw new Error("Age must be a positive number");
       }
     },
-  }
+  },
+  tokens: [{
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 })
 
+userSchema.methods.generateAuthToken = async function() {
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, 'thisissang');
+
+  user.tokens = user.tokens.concat({token});
+  await user.save();
+
+  return token;
+}
+
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+  
+  if(!user) {
+    throw new Error('Unable to login');
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if(!isMatch) {
+    throw new Error('Unable to login');
+  }
+
+  return user;
+}
+
+//hash password
 userSchema.pre('save', async function(next) {
   const user = this;
   if(user.isModified('password')) {
--- src/routers/user.js
+++ src/routers/user.js
@@ -4,13 +4,24 @@ const router = new express.Router();
 
 router.post("/users", async (req, res) => {
   const user = new User(req.body);
+  const token = await user.generateAuthToken();
   try {
     await user.save();
-    res.status(201).send(user);
+    res.status(201).send({ user, token });
   } catch (err) {
     res.status(400).send(err);
   }
 });
+
+router.post('/users/login', async (req, res) => {
+  try {
+    const user = await User.findByCredentials(req.body.email, req.body.password);
+    const token = await user.generateAuthToken();
+    res.send({user, token})
+  } catch(e) {
+    res.status(400).send();
+  }
+})
 
 router.patch("/users/:id", async (req, res) => {
   const updates = Object.keys(req.body);
